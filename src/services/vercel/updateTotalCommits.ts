d3d6c5b0eaa080cb018a6a097046ba7f1e6b9111
Beast <shadman.afzal.7@gmail.com>
2025-03-02 14:56:28 +0530

    add: check and logs for cron job

--- src/services/vercel/updateTotalCommits.ts
+++ src/services/vercel/updateTotalCommits.ts
@@ -4,8 +4,8 @@ const PROJECT_ID = process.env.PROJECT_ID!;
 const VERCEL_API_TOKEN = process.env.VERCEL_API_TOKEN!;
 const ENVIRONMENT_KEY = process.env.ENVIRONMENT_KEY!;
 
-const updateCommitCount = (totalCommits: number) => {
-  return axios.patch(
+const updateCommitCount = async (totalCommits: number) => {
+  await axios.patch(
     `https://api.vercel.com/v9/projects/${PROJECT_ID}/env/${ENVIRONMENT_KEY}`,
     {
       key: 'NEXT_PUBLIC_GITHUB_TOTAL_COMMITS',
@@ -19,6 +19,7 @@ const updateCommitCount = (totalCommits: number) => {
       },
     }
   );
+  console.log('Total commits updated successfully');
 };
 
 export default updateCommitCount;
